889293d794fd1b1f5ec7cc261cd67e6b43365c55
pofu.lu <pofu.lu@outlook.com>
2020-05-14 22:42:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# Transfer
+
+![index](https://github.com/pofulu/sparkar-transfer/blob/master/README.assets/index.gif?raw=true)
+
+**Transfer** is a Spark AR tool to send any object's transform value to Patch Editor by naming, without additional script.
+
+
+
+## Install
+
+### Import
+
+0. [Download Transfer.js](https://raw.githubusercontent.com/pofulu/sparkar-transfer/master/TransferDemo/scripts/Transfer.js) (Right click and Save as)
+
+2. Drag/Drop or import it to Spark AR
+
+3. (Optional) Load in the required modules
+
+    ```javascript
+    const Transfer = require('./Transfer');
+    // Your script...
+    ```
+
+4. You can also [Click Here to Download a Sample Project](https://yehonal.github.io/DownGit/#home?url=https://github.com/pofulu/sparkar-transfer/tree/master/TransferDemo).
+
+### npm
+
+0. Add package with `yarn` or `npm`
+
+    ```shell
+    yarn add sparkar-transfer
+    ```
+
+    or
+
+    ```shell
+    npm i sparkar-transfer
+    ```
+
+1. Load in the required modules. If you use webpack to transpile code, you must import this module.
+
+    ```javascript
+    const Transfer = require('sparkar-transfer');
+    // Your script...
+    ```
+
+
+
+## Usage 
+
+This tool make object follow head by **naming**:
+
+1. Add the keyword `#` to the name of object that you want to tranfer value to Patch Editor.
+
+2. Select any script and add the desired variables to **From Script**: 
+
+    | Variable Pattern | Type|Description                                                  |
+    | ---------------- | ------------------------------------------------------------ | ------------------------------------------------------------ |
+    | `name_p`         | `Vector3`                                    |The position value of the `name` object.|
+    | `name_s`         | `Vector3` |The scale value of the `name` object.|
+    | `name_r`         | `Vector3` |The rotation value of the `name` object, **in degree.**|
+    | `name_wp`        | `Vector3` |The world position value of the `name` object.|
+    | `name_ws`        | `Vector3` |The world scale value of the `name` object.|
+    | `name_wr`        | `Vector3` |The world rotation value of the `name` object.|
+    | `name_pp`        | `Vector2` |The position value in screen percentage of the `name` object. Please refer [Here](https://github.com/pofulu/Spark-AR-PFTools/tree/master/PFScreen) for more details.|
+
+Please note that you should avoid using the same base name. For example, these object named `name #`, `name   #`, `# name ` or `#  name ` will cause conflict in Patch variables.
+
+
+
+## Additional
+
+- You can use `Transfer.logTrackedObjectNames()` to log all tracked objects.
+
---
+++ TransferDemo/scripts/Transfer.js
@@ -0,0 +1,100 @@
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– CONFIG
+
+const TRANSFER_KEYWORD = '#';
+
+const FROM_SCRIPT_PATTERN = 'NAME_KEY';    //i.e.: plane0_p
+
+const POSITION_KEYWORD = 'p';
+const SCALE_KEYWORD = 's';
+const ROTATION_KEYWORD = 'r';
+
+const SCREEN_POSITION_KEYWORD = 'pp';
+
+const WORLD_POSITION_KEYWORD = 'wp';
+const WORLD_SCALE_KEYWORD = 'ws';
+const WORLD_ROTATION_KEYWORD = 'wr';
+
+//–––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+const Scene = require('Scene');
+const Reactive = require('Reactive');
+const Patches = require('Patches');
+const Diagnostics = require('Diagnostics');
+const CameraInfo = require('CameraInfo');
+const Units = require('Units');
+
+const targets = Scene.root.findByPath(`**/*${TRANSFER_KEYWORD}*`);
+const R2D = 57.2957795131;
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– Method
+
+/**
+ * @returns {Promise<void>}
+ */
+targets.then(results => {
+    const getName = sceneObject => sceneObject.name.replace(`${TRANSFER_KEYWORD}`, '').trim();
+
+    groupBy(results, i => getName(i)).forEach(element => {
+        if (element.length > 1) {
+            let nameList = element.map(i => `"${i.name}"`);
+            nameList[nameList.length - 1] += '  <--';
+            nameList = nameList.join('\n');
+
+            Diagnostics.log(`More than one object's origin name are "${getName(element[0])}", the value sent to patch will be only the last one:\n${nameList}`);
+        }
+    });
+
+    const setAllFromScript = results.map(target => {
+        const name = getName(target);
+        const pattern = FROM_SCRIPT_PATTERN.replace('NAME', name);
+
+        return Promise.all([
+            Patches.inputs.setPoint(pattern.replace('KEY', POSITION_KEYWORD), target.transform.position),
+            Patches.inputs.setPoint(pattern.replace('KEY', SCALE_KEYWORD), Reactive.pack3(target.transform.scaleX, target.transform.scaleY, target.transform.scaleZ)),
+            Patches.inputs.setPoint(pattern.replace('KEY', ROTATION_KEYWORD), Reactive.pack3(target.transform.rotationX.mul(R2D), target.transform.rotationY.mul(R2D), target.transform.rotationZ.mul(R2D))),
+
+            Patches.inputs.setPoint(pattern.replace('KEY', SCREEN_POSITION_KEYWORD), Reactive.pack3(getPercX(target.transform.x), getPercY(target.transform.y), 0)),
+
+            Patches.inputs.setPoint(pattern.replace('KEY', WORLD_POSITION_KEYWORD), target.worldTransform.position),
+            Patches.inputs.setPoint(pattern.replace('KEY', WORLD_SCALE_KEYWORD), Reactive.pack3(target.worldTransform.scaleX, target.worldTransform.scaleY, target.worldTransform.scaleZ)),
+            Patches.inputs.setPoint(pattern.replace('KEY', WORLD_ROTATION_KEYWORD), Reactive.pack3(target.worldTransform.rotationX.mul(R2D), target.worldTransform.rotationY.mul(R2D), target.worldTransform.rotationZ.mul(R2D))),
+        ]);
+    })
+
+    return Promise.all(setAllFromScript);
+});
+
+module.exports = new class TransferModule {
+    logTrackedObjectNames() {
+        return targets.then(results => Diagnostics.log(results.map(i => i.name)));
+    }
+}
+
+//––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––– Utility
+
+function groupBy(array, f) {
+    let groups = {};
+    array.forEach(function (o) {
+        let group = JSON.stringify(f(o));
+        groups[group] = groups[group] || [];
+        groups[group].push(o);
+    });
+    return Object.keys(groups).map(function (group) {
+        return groups[group];
+    });
+}
+
+/**
+ * @returns {ScalarSignal}
+ */
+function getPercX(transformX) {
+    const widthMax = CameraInfo.previewSize.width.div(CameraInfo.previewSize.height).mul(Units.cm(25));
+    return transformX.add(widthMax).div(widthMax.mul(2));
+}
+
+/**
+ * @returns {ScalarSignal}
+ */
+function getPercY(transformY) {
+    return Reactive.val(Units.cm(25)).sub(transformY).div(Units.cm(50));
+}
